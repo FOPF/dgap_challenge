7484387bef399ea1537cc96aff5c3ee6a131adbf
Pavel <pavel.semenenko@phystech.edu>
2016-11-04 07:38:13 +0300

    Fixed VK support

--- dgap_challenge/urls.py
+++ dgap_challenge/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^dota/', include('dota.urls', namespace='dota')),
     url(r'^login/', include('social.apps.django_app.urls', namespace='social')),
-    url(r'^', include('dota.urls', namespace='dota')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
 ]
