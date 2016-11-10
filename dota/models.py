f8cc131a162e17b2e0729ac98c2ebfd3ce3afcb6
Pavel <pavel.semenenko@phystech.edu>
2016-11-10 23:32:53 +0300

    Added models for tournament

--- dota/models.py
+++ dota/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ObjectDoesNotExist
 
 class Article(models.Model):
     title = models.CharField(max_length=255) # заголовок поста
@@ -37,3 +37,44 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
+
+
+class Tournament(models.Model):
+    name = models.CharField('Название чемпионата', max_length=100)
+    rules = models.TextField('Правила чемпионата')
+    start_dttm = models.DateTimeField('Начало чемпионата')
+    end_dttm = models.DateTimeField('Конец чемпионата')
+
+
+class TournamentRound(models.Model):
+    tournament = models.ForeignKey(Tournament)
+    num = models.IntegerField('Номер раунда')
+    name = models.CharField('Название раунда', max_length=40, default=True, blank=True, null=True)
+    start_dttm = models.DateTimeField('Начало раунда')
+    end_dttm = models.DateTimeField('Конец раунда')
+
+
+class TournamentGame(models.Model):
+    round = models.ForeignKey(TournamentRound)
+    team1 = models.ForeignKey(Team)
+    team2 = models.ForeignKey(Team)
+    is_active = models.BooleanField("Игра идет")
+    score = models.CharField('Результат', max_length=40, blank=True, null=True, default=None)
+    winner = models.ForeignKey(Team, blank=True, null=True)
+    teams = models.ManyToManyField(Team)
+
+    @property
+    #TODO self.winner == None => CRASH!!!
+    def loser(self):
+        if self.is_active:
+            return None
+        try:
+            if self.winner == self.team1:
+                return self.team2
+            return self.team1
+        except ObjectDoesNotExist:
+            return None
+
+    def set_results(self, winner, score):
+        self.winner, self.score = winner, score
+        self.save()
